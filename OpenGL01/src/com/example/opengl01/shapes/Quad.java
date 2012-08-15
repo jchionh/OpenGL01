7950edb7a63b84b0819e3c2c5aeb1665909694d9
Jason Chionh <jasonchionh@gmail.com>
2012-08-14 23:16:37 -0700

    - added vertex colors to the quad

--- OpenGL01/src/com/example/opengl01/shapes/Quad.java
+++ OpenGL01/src/com/example/opengl01/shapes/Quad.java
@@ -16,6 +16,13 @@ public class Quad {
 			 1.0f,  1.0f, 0.0f,  // 3, Top Right
 	};
 	
+	private float[] vertexColors = {
+			 1.0f,  0.0f, 0.0f, 1.0f, // 0, Top Left
+			 0.0f,  1.0f, 0.0f, 1.0f, // 1, Bottom Left
+			 0.0f,  0.0f, 1.0f, 1.0f, // 2, Bottom Right
+			 1.0f,  1.0f, 0.0f, 1.0f  // 3, Top Right
+	};
+	
 	// the order we're connecting them
 	private short[] indices = { 0, 1, 3, 2 };
 	
@@ -24,6 +31,7 @@ public class Quad {
 	// here's the vertex buffer
 	private FloatBuffer vtxBuffer;
 	private ShortBuffer idxBuffer;
+	private FloatBuffer clrBuffer;
 	
 	
 	// --------------------------------------------------------
@@ -37,11 +45,17 @@ public class Quad {
 		vtxBuffer.put(vertices);
 		vtxBuffer.position(0);
 		
-		ByteBuffer idxBB = ByteBuffer.allocateDirect(vertices.length * 2);
+		ByteBuffer idxBB = ByteBuffer.allocateDirect(indices.length * 2);
 		idxBB.order(ByteOrder.nativeOrder());
 		idxBuffer = idxBB.asShortBuffer();
 		idxBuffer.put(indices);
 		idxBuffer.position(0);
+		
+		ByteBuffer clrBB = ByteBuffer.allocateDirect(vertexColors.length * 4);
+		clrBB.order(ByteOrder.nativeOrder());
+		clrBuffer = clrBB.asFloatBuffer();
+		clrBuffer.put(vertexColors);
+		clrBuffer.position(0);
 		
 	}
 	
@@ -71,6 +85,10 @@ public class Quad {
 		// cull back
 		gl.glCullFace(GL10.GL_BACK);
 		
+		// enable the color buffer mode
+		gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+		gl.glColorPointer(4, GL10.GL_FLOAT, 0, clrBuffer);
+		
 		// enble the vtx array
 		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
 		// give it the vtx buffer
@@ -78,6 +96,7 @@ public class Quad {
 		// now use it to draw the elements
 		gl.glDrawElements(GL10.GL_TRIANGLE_STRIP, indices.length, GL10.GL_UNSIGNED_SHORT, idxBuffer);
 		// now disable what we enabled before
+		gl.glDisableClientState(GL10.GL_COLOR_ARRAY);
 		gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
 		// disable the culling
 		gl.glDisable(GL10.GL_CULL_FACE);
